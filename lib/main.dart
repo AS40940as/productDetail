26dafb7d87396f3ff07aaf8b16df10ba418ec5ae
1380alis <1380as1380@gmail.com>
2024-09-17 14:56:04 +0330

    .

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'services/api_service.dart';
+import 'models/product.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Product App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductListScreen(),
+    );
+  }
+}
+
+class ProductListScreen extends StatefulWidget {
+  @override
+  _ProductListScreenState createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  late Future<List<Product>> futureProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProducts = ApiService().fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 118, 193, 255),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+      body: FutureBuilder<List<Product>>(
+        future: futureProducts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No products found'));
+          } else {
+            final products = snapshot.data!;
+            return ListView.separated(
+              padding: EdgeInsets.all(10),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ListTile(
+                  tileColor: Color.fromARGB(255, 191, 217, 218),
+                  leading: Image.network(
+                    product.image,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(product.title),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ProductDetailScreen(product: product),
+                      ),
+                    );
+                  },
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+            );
+          }
+        },
+      ),
+
+      // NavigationBar
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 255, 164, 210),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 15, 57, 121),
+              ),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 102, 196, 233),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                accountName: Text(
+                  "Ali Saeedi",
+                  style: TextStyle(fontSize: 18),
+                ),
+                accountEmail: Text("1380as1380@gmail.com"),
+                currentAccountPictureSize: Size.square(40),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 165, 255, 137),
+                  child: Text(
+                    "A",
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        color: Color.fromARGB(255, 15, 57, 121)),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.person,
+                color: Color.fromARGB(255, 17, 75, 122),
+              ),
+              title: const Text(' My Profile '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ProductDetailScreen
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  ProductDetailScreen({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 80, 223, 241),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.network(
+              product.image,
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              product.category,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,71 @@
+// To parse this JSON data, do
+//
+//     final Product = ProductFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Product> productFromJson(String str) =>
+    List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Product {
+  int id;
+  String slug;
+  String url;
+  String title;
+  String content;
+  String image;
+  String thumbnail;
+  String status;
+  String category;
+  String publishedAt;
+  String updatedAt;
+  int userId;
+
+  Product({
+    required this.id,
+    required this.slug,
+    required this.url,
+    required this.title,
+    required this.content,
+    required this.image,
+    required this.thumbnail,
+    required this.status,
+    required this.category,
+    required this.publishedAt,
+    required this.updatedAt,
+    required this.userId,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        slug: json["slug"],
+        url: json["url"],
+        title: json["title"],
+        content: json["content"],
+        image: json["image"],
+        thumbnail: json["thumbnail"],
+        status: json["status"],
+        category: json["category"],
+        publishedAt: json["publishedAt"],
+        updatedAt: json["updatedAt"],
+        userId: json["userId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "slug": slug,
+        "url": url,
+        "title": title,
+        "content": content,
+        "image": image,
+        "thumbnail": thumbnail,
+        "status": status,
+        "category": category,
+        "publishedAt": publishedAt,
+        "updatedAt": updatedAt,
+        "userId": userId,
+      };
+}
